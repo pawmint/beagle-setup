5aac4fbff00f89d6f2354e913cd6082482075e5d
Mickael_Germain <germain.mickael@gmail.com>
2014-06-26 18:02:44 +0200

    cleaning

--- onBoardSetup/timesync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ntpdate -u 0.sg.pool.ntp.org # To syncronize the clock, write: sg in Singapore, and fr in France
-echo Asia/Singapore > /etc/timezone # To etablish the correct timezone, write Asia/singapore or Europe/Paris. You can find the all list with the command "ls /usr/share/zoneinfo/<continent>/<country>"
-
