00bba5d623dfe64099773db8bbc3138895053951
Mickael_Germain <germain.mickael@gmail.com>
2014-06-29 11:17:12 +0200

    Adding git pull

--- init.pp
+++ init.pp
@@ -3,13 +3,13 @@ vcsrepo{"/usr/local/etc/puppet":
 	ensure => present,
 	provider => git,
 	source => 'git://github.com/pawmint/beagle-setup.git',
-	revision => 'puppet',
+	revision => 'puppetDir',
 }
 
 cron{"puppet":
 	require => Vcsrepo["/usr/local/etc/puppet"],
 	ensure => present,
-	command => '/usr/bin/puppet apply --modulepath /usr/local/etc/puppet/modules /usr/local/etc/puppet/manifests/node.pp',
+	command => '(cd /usr/local/etc/puppet && git pull && /usr/bin/puppet apply --modulepath /usr/local/etc/puppet/modules /usr/local/etc/puppet/manifests/node.pp)',
 	user => root,
 	minute => '*/10',
 }
