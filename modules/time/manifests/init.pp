31e61dbbc447ddfc24b9558a70c7ffd6ad141a13
Mickael_Germain <germain.mickael@gmail.com>
2014-06-29 14:22:48 +0200

    require bug

--- modules/time/manifests/init.pp
+++ modules/time/manifests/init.pp
@@ -1,7 +1,7 @@
  
 class time {
 	package { 'tzdata' :
-		require Class['::ntp'],
+		require => Class['::ntp'],
 		ensure => installed,
 	}
 
@@ -9,11 +9,5 @@ class time {
 		servers => [ '0.pool.ntp.org', '1.pool.ntp.org', '2.pool.ntp.org', '3.pool.ntp.org' ],
 	}
 
-	puppi::netinstall { 'mochad' :
-		require => Package['libusb-1.0-0-dev'],
-		url => 'http://sourceforge.net/projects/mochad/files/mochad-0.1.16/download',
-		extracted_dir => 'mochad-0.1.16',
-		destination_dir => '/tmp',
-		postextract_command => '/tmp/mochad-0.1.16/configure && make && make install',
-	}
+	
 }
