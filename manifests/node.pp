140cf662f58a67df09d39bfd29048061fd9b13a6
Mickael_Germain <germain.mickael@gmail.com>
2014-06-29 12:55:14 +0200

    update with new packages

--- manifests/node.pp
+++ manifests/node.pp
@@ -1,13 +1,21 @@
  
 node default {
+	
+	include time
+	
 	class { 'gsm_network':
 		provider => 'free',
 	}
 
 	include mochad
 
+	class {'reverse_proxy':
+		url => ubuntu@ec2-54-186-251-30.us-west-2.compute.amazonaws.com,
+        port => 5900,
+    }
+
 	class { 'python' :
-		version => '3'
+		version => '3',
 		pip => true,
 		dev => true,
 		virualenv => true,
